deab058cc6f4eba2cb3c3af96710e7aeac27e8d8
Samantha Bobra <sam.bobra@gmail.com>
2015-01-31 00:05:43 -0800

    Attempt to remove ugly borders from header and footer.

--- www.sambobra.com/web/content/wp-content/themes/themify-base-child/functions.php
+++ www.sambobra.com/web/content/wp-content/themes/themify-base-child/functions.php
@@ -1,4 +1,9 @@
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-		wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+<?php
+/**
+* Enqueues child theme stylesheet, loading first the parent theme stylesheet.
+*/
+function themify_custom_enqueue_child_theme_styles() {
+	wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
 }
+add_action( 'wp_enqueue_scripts', 'themify_custom_enqueue_child_theme_styles', 11 );
