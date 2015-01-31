2d16f3be54a5fc8a340f39646959434f0c3e3d14
Samantha Bobra <sam.bobra@gmail.com>
2015-01-31 00:35:57 -0800

    Fix to get the child theme CSS to actually work.

--- www.sambobra.com/web/content/wp-content/themes/themify-base-child/functions.php
+++ www.sambobra.com/web/content/wp-content/themes/themify-base-child/functions.php
@@ -4,6 +4,6 @@
 * Enqueues child theme stylesheet, loading first the parent theme stylesheet.
 */
 function themify_custom_enqueue_child_theme_styles() {
-	wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'parent-theme-css', get_stylesheet_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'themify_custom_enqueue_child_theme_styles', 11 );
